108d58cfeeada8312283669b501c36ea7213f9ce
kevinleonel.abundis.contractor <kevinleonel.abundis.contractor@bbva.com>
2023-02-20 11:32:33 -0600

    Se modifican repositorios con Spring Data JPA

--- src/main/java/org/abundis/test/springboot/app/Datos.java
+++ src/main/java/org/abundis/test/springboot/app/Datos.java
@@ -4,6 +4,7 @@ import org.abundis.test.springboot.app.models.Banco;
 import org.abundis.test.springboot.app.models.Cuenta;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 public class Datos {
 //    public static final Cuenta CUENTA_001 = new Cuenta(1L, "Andrés", new BigDecimal("1000"));
@@ -11,15 +12,15 @@ public class Datos {
 //    public static final Banco BANCO = new Banco(1L, "BBVA", 0);
 
 
-    public static Cuenta crearCuenta001(){
-        return new Cuenta(1L, "Andrés", new BigDecimal("1000"));
+    public static Optional<Cuenta> crearCuenta001(){
+        return Optional.of(new Cuenta(1L, "Andrés", new BigDecimal("1000")));
     }
 
-    public static Cuenta crearCuenta002(){
-        return new Cuenta(2L, "Jhon", new BigDecimal("2000"));
+    public static Optional<Cuenta> crearCuenta002(){
+        return Optional.of(new Cuenta(2L, "Jhon", new BigDecimal("2000")));
     }
 
-    public static Banco crearBanco(){
-        return new Banco(1L, "BBVA", 0);
+    public static Optional<Banco> crearBanco(){
+        return Optional.of(new Banco(1L, "BBVA", 0));
     }
 }
--- src/test/java/org/abundis/test/springboot/app/SpringbootTestApplicationTests.java
+++ src/test/java/org/abundis/test/springboot/app/SpringbootTestApplicationTests.java
@@ -68,7 +68,7 @@ class SpringbootTestApplicationTests {
 
 		verify(cuentaRepository, times(3)).findById(1L);
 		verify(cuentaRepository, times(3)).findById(2L);
-		verify(cuentaRepository, times(2)).update(any(Cuenta.class));
+		verify(cuentaRepository, times(2)).save(any(Cuenta.class));
 
 		verify(bancoRepository, times(2)).findById(1L);
 		verify(bancoRepository).update(any(Banco.class));
@@ -103,7 +103,7 @@ class SpringbootTestApplicationTests {
 
 		verify(cuentaRepository, times(3)).findById(1L);
 		verify(cuentaRepository, times(2)).findById(2L);
-		verify(cuentaRepository,never()).update(any(Cuenta.class));
+		verify(cuentaRepository,never()).save(any(Cuenta.class));
 
 		verify(bancoRepository, times(1)).findById(1L);
 		verify(bancoRepository, never()).update(any(Banco.class));
